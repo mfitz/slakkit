65bc89c16775fe12221af1068996fc86d7cccf88
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2021-07-17 22:03:55 +0100

    Fix bad env var names and broken Slack API call

--- main.py
+++ main.py
@@ -18,62 +18,62 @@ def get_secret(secret_name):
         raise ValueError("Did not find an appropriate secret under the name '{}'".format(secret_name))
 
 
-def make_slack_message(reddit):
-    return {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "> {}".format(reddit['data']['title'])
-                }
-            },
-            {
-                "type": "image",
-                "image_url": "{}".format(reddit['data']['url_overridden_by_dest']),
-                "alt_text": "marg"
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Scraped from Reddit's <{}|{}>.".format(
-                        'https://www.reddit.com{}'.format(reddit['data']['permalink']),
-                        reddit['data']['subreddit'])
-                }
-            },
-            {
-                "type": "divider"
+def make_slack_message_blocks(reddit):
+    return [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "> {}".format(reddit['data']['title'])
             }
-        ]
-    }
+        },
+        {
+            "type": "image",
+            "image_url": "{}".format(reddit['data']['url_overridden_by_dest']),
+            "alt_text": "marg"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Pulled from Reddit's <{}|{}>.".format(
+                    'https://www.reddit.com{}'.format(reddit['data']['permalink']),
+                    reddit['data']['subreddit'])
+            }
+        },
+        {
+            "type": "divider"
+        }
+    ]
 
 
-def send_slack_message(channel, api_token, attachments=None, blocks=None):
+def send_slack_message(channel, api_token, blocks):
     print('Sending slack message to {} channel...'.format(channel))
-    response = slack.SlackClient(api_token).api_call(
-        "chat.postMessage",
+    response = slack.WebClient(api_token).chat_postMessage(
         channel=channel,
-        attachments=attachments,
-        blocks=blocks,
-        user="slackit",
+        blocks=blocks
     )
     print('Slack API response {}'.format(response))
 
 
 def get_random_reddits(subreddit_list):
     subreddit = random.choice(subreddit_list)
-    print("Retrieving top 10 posts for the day from {}".format(subreddit))
-    url = "https://www.reddit.com/r/{}/top.json?limit=10&t=day".format(subreddit)
+    print("Retrieving top posts from {}".format(subreddit))
+    url = "https://www.reddit.com/r/{}/top.json?limit=30&t=month".format(subreddit)
     print("Requesting {}".format(url))
-    response = requests.get(url, header={'User-agent': 'SlackIt 0.1'})
+    response = requests.get(url, headers={'User-agent': 'SlackIt 0.1'})
     print("Got response {}".format(response))
     return response.json()
 
 
 def choose_a_reddit(reddit_list):
+    print("Selecting a reddit from a list of {}".format(len(reddit_list)))
+    random.shuffle(reddit_list)
     for reddit in reddit_list:
-        if reddit['data']['title'] and reddit['data']['url_overridden_by_dest']:
+        if reddit['data']['title'] \
+                and reddit['data']['url_overridden_by_dest'] \
+                and not reddit['data']['is_video'] \
+                and ('is_gallery' not in reddit['data'] or not reddit['data']['is_gallery']):
             print("Selected a reddit from the list of reddits: {}".format(reddit))
             return reddit
     raise ValueError('Could not find a reddit with a title and picture from this list of reddits: {}'
@@ -82,12 +82,12 @@ def choose_a_reddit(reddit_list):
 
 if __name__ == '__main__':
     print("Reading SlackIt configuration from env vars....")
-    target_channel = os.environ.get('slackit.TARGET_CHANNEL')
-    oauth_secret_name = os.environ.get('slackit.OAUTH_SECRET_NAME')
-    slack_oauth_token = get_secret(oauth_secret_name)
-    subreddit_list = os.environ.get('slackit.SUBREDDIT_LIST').split(',')
+    target_channel = os.environ.get('slackit_TARGET_CHANNEL')
+    oauth_secret_name = os.environ.get('slackit_OAUTH_SECRET_NAME')
+    slack_oauth_token = get_secret(oauth_secret_name).get('api_key')
+    subreddit_list = os.environ.get('slackit_SUBREDDIT_LIST').split(',')
 
-    reddit_json = get_random_reddits(subreddit_list)
-    reddit = choose_a_reddit(subreddit_list['data']['children'])
-    slack_blocks = make_slack_message(reddit)
+    reddits = get_random_reddits(subreddit_list)
+    reddit = choose_a_reddit(reddits['data']['children'])
+    slack_blocks = make_slack_message_blocks(reddit)
     send_slack_message(target_channel, slack_oauth_token, blocks=slack_blocks)
