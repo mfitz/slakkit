c6d73db42d48281d838c8e211e849d9a420270f8
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2021-10-31 20:48:12 +0000

    Add more unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,6 +19,25 @@ def some_reddit():
     }
 
 
+@pytest.fixture()
+def subreddit_list():
+    yield ['lions', 'tigers', 'bears']
+
+
+@pytest.fixture()
+def a_good_reddit_image_post():
+    yield {
+        "kind": "t3",
+        "data": {
+            "subreddit": "CatsInBusinessAttire",
+            "title": "A stray showed up next door with the snazziest striped tie.",
+            "post_hint": "image",
+            "url_overridden_by_dest": "https://i.redd.it/am25oz0yafv71.jpg",
+            "is_video": False
+        }
+    }
+
+
 def test_propagates_errors_when_sending_slack_messages(mocker):
     slack_error = SlackApiError("OOPSY DAISY!", 'some-response')
     mocker.patch.object(slack.WebClient, 'chat_postMessage', side_effect=slack_error)
@@ -165,3 +184,74 @@ def test_extracts_posts_list_from_subreddit_api_responses(mocker):
 
     assert top_posts == []
     mock_response.json.assert_called_once()
+
+
+def test_randomly_chooses_subreddit_to_query(mocker, subreddit_list):
+    chosen_subreddit = subreddit_list[-1]
+    mocker.patch.object(main.random, 'choice', return_value=chosen_subreddit)
+    mocker.patch.object(main, 'get_top_posts', return_value=[{}] * 50)
+
+    main.get_random_reddits(subreddit_list)
+
+    main.random.choice.assert_called_once_with(subreddit_list)
+    main.get_top_posts.assert_called_once_with(chosen_subreddit, 50, 'month')
+
+
+def test_overrides_default_reddit_api_page_size_with_optional_env_var(mocker, subreddit_list):
+    mocker.patch.object(main, 'get_top_posts', return_value=[{}] * 50)
+    os.environ["slakkit_REDDIT_PAGE_SIZE"] = '3'
+    try:
+        main.get_random_reddits(subreddit_list)
+
+        main.get_top_posts.assert_called_once_with(mocker.ANY, 3, 'month')
+    finally:
+        del os.environ['slakkit_REDDIT_PAGE_SIZE']
+
+
+def test_requests_more_data_when_reddit_returns_too_few_posts(mocker, subreddit_list):
+    chosen_subreddit = subreddit_list[-1]
+    mocker.patch.object(main.random, 'choice', return_value=chosen_subreddit)
+    mocker.patch.object(main, 'get_top_posts', return_value=[{}])
+
+    main.get_random_reddits(subreddit_list)
+
+    main.get_top_posts.assert_has_calls([
+        mocker.call(chosen_subreddit, 50, 'month'),
+        mocker.call(chosen_subreddit, 50, 'year')
+    ])
+
+
+def test_raises_error_when_no_suitable_reddit_image_is_available():
+    with pytest.raises(ValueError) as raised_error:
+        main.choose_a_reddit([])
+    assert raised_error.value.args[0] == 'Could not find a suitable image post from this list of reddits: []'
+
+
+def test_choose_an_acceptable_image_post_when_available(a_good_reddit_image_post):
+    chosen_post = main.choose_a_reddit([a_good_reddit_image_post])
+
+    assert chosen_post == a_good_reddit_image_post
+
+
+def test_ignores_image_posts_that_are_videos(a_good_reddit_image_post):
+    a_good_reddit_image_post['data']['is_video'] = True
+    with pytest.raises(ValueError) as raised_error:
+        main.choose_a_reddit([a_good_reddit_image_post])
+    assert raised_error.value.args[0] == \
+           'Could not find a suitable image post from this list of reddits: {}'.format([a_good_reddit_image_post])
+
+
+def test_ignores_image_posts_that_are_galleries(a_good_reddit_image_post):
+    a_good_reddit_image_post['data']['is_gallery'] = True
+    with pytest.raises(ValueError) as raised_error:
+        main.choose_a_reddit([a_good_reddit_image_post])
+    assert raised_error.value.args[0] == \
+           'Could not find a suitable image post from this list of reddits: {}'.format([a_good_reddit_image_post])
+
+
+def test_ignores_image_posts_that_are_gifs(a_good_reddit_image_post):
+    a_good_reddit_image_post['data']['is_gif'] = True
+    with pytest.raises(ValueError) as raised_error:
+        main.choose_a_reddit([a_good_reddit_image_post])
+    assert raised_error.value.args[0] == \
+           'Could not find a suitable image post from this list of reddits: {}'.format([a_good_reddit_image_post])
